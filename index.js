debc4b9f5c167e637efb270a0281e1a1e62a8d62
Hom Yan <yanzhihong23@qq.com>
2016-05-23 18:27:08 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('angular.img.placeholder')
+    .directive('httpSrc', ['$http', function($http) {
+      var directive = {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+          if (attrs.placeholder) {
+            attrs.$set('src', attrs.placeholder);
+          }
+
+          attrs.$observe('httpSrc', function(url) {
+            if (!url) return;
+
+            $http({
+              method: 'GET',
+              url: url,
+              responseType: 'arraybuffer',
+              cache: 'true'
+            }).then(function(res) {
+              var blob = new Blob(
+                [res.data], { type: res.headers('Content-Type') }
+              );
+
+              attrs.$set('src', URL.createObjectURL(blob));
+            });
+          });
+
+        }
+      };
+
+      return directive;
+    }]);
+})();
